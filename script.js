76088d436789067f5c8e1c936bd6c419c0bb7f51
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-23 15:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ submit.addEventListener("click", (event) => {
 	event.preventDefault();
 	let radius = document.getElementById("radius").value;
 	let volume = Math.PI * (4/3) * radius ** 3;
-
+	let ans = volume.toFixed(4);
+	
 	let volumeIn = document.getElementById("volume");
-	volumeIn.value = volume;
+	volumeIn.value = ans;
 })
